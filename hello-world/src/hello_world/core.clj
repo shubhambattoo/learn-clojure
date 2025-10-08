337784a85c77999ba57c3b113eab193e2eb0711b
Shubham Battoo <shubham.battoo@arcticwolf.com>
2025-10-08 19:03:00 +0530

    data structures

--- hello-world/src/hello_world/core.clj
+++ hello-world/src/hello_world/core.clj
@@ -1,8 +1,40 @@
 (ns hello-world.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as string]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
-  
+  (println "Hello, World!"
+           ;; (slurp "/usr/share/dict/connectives")
+           ))
+[1 2 3] ;; vector
+{:a 1 :b 2} ;; map
+#{:a :b} ;; set
+'(1 2 3) ;; list
+
+(println (string/upper-case "hello"))
+
+(def names (string/split-lines (slurp "/usr/share/dict/propernames")))
+
+names
+
+(def mangle
+  (fn [string]
+    (string/join "-" (string/reverse string))))
+
+;; (println(mangle "hello"))
+
+
+(defn demangle [mangle-string]
+  (string/reverse (string/replace mangle-string "-" "")))
+
+;; (println (demangle "o-l-l-e-h"))
+
+
+(defn palindrome? [word]
+  (let [lower-case (string/lower-case word)
+        reversed (string/reverse lower-case)]
+    (= reversed lower-case)))
+
+;; (println (palindrome? "eYe"))
